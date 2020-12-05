1e268f31736c184ecc1e3d23552a7648393fb298
janbubenik <68082574+janbubenik@users.noreply.github.com>
2020-12-05 23:27:23 +0100

    commit

--- js/slick-carousel.js
+++ js/slick-carousel.js
@@ -2,6 +2,8 @@
   $(document).ready(function() {
       $('.slider-header, .slider-about, .slider-work, .slider-technology, .slider-team').slick({
           infinite: true,
+          autoplay: true,
+          autoplaySpeed: 3500,
           dots: true,
           speed: 400,
           prevArrow: false,
